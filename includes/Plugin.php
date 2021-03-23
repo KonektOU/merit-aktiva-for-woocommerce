c9116a8d451d20d3c663bd9c6ec20bbda610fac5
Risto Niinemets <risto.niinemets@gmail.com>
2021-03-23 10:19:38 +0200

    Bump version to 1.0.16

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -22,7 +22,7 @@ class Plugin extends Framework\SV_WC_Plugin {
 	protected static $instance;
 
 	/** plugin version number */
-	const VERSION = '1.0.15';
+	const VERSION = '1.0.16';
 
 	/** plugin id */
 	const PLUGIN_ID = 'wc-merit-aktiva';
