2241e09b4c81877bc0101208e0467398ff003bcf
Risto Niinemets <risto.niinemets@gmail.com>
2022-12-01 08:39:29 +0200

    Revert coupons logic

--- includes/API.php
+++ includes/API.php
@@ -88,12 +88,6 @@ class API extends Framework\SV_WC_API_Base {
 		$order_line_items = [];
 		$total_amount     = 0;
 		$total_tax_amount = 0;
-		$giftcard         = null;
-		$has_coupons      = false;
-
-		if ( ! empty( $coupons = $order->get_coupons() ) ) {
-			$has_coupons = true;
-		}
 
 		// Look for location code from shipping method
 		$location_code = $this->get_plugin()->get_order_warehouse_id( $order );
@@ -111,7 +105,7 @@ class API extends Framework\SV_WC_API_Base {
 			$order_line_items = $order->get_items( [ 'line_item', 'shipping', 'fee' ] );
 		}
 		elseif ( empty( $order_line_items ) ) {
-			$order_line_items = $order->get_items( [ 'line_item', 'shipping', 'fee', 'coupon' ] );
+			$order_line_items = $order->get_items( [ 'line_item', 'shipping', 'fee' ] );
 		}
 
 		// Find matching location
@@ -123,20 +117,8 @@ class API extends Framework\SV_WC_API_Base {
 		}
 
 		// Add order items
-		/** @var \WC_Order_Item_Product|\WC_Order_Item_Coupon|\WC_Order_Item_Shipping $order_item */
+		/** @var \WC_Order_Item_Product $order_item */
 		foreach ( $order_line_items as $order_item ) {
-
-			if ( $order_item->is_type( 'coupon' ) ) {
-				$item_price = 0 - $order_item->get_discount( 'edit' );
-				$total_tax  = 0 - $order_item->get_discount_tax( 'edit' );
-			} else {
-				$item_price = $order_item->get_total( 'edit' );
-				$total_tax  = $order_item->get_total_tax();
-
-				if ( $has_coupons && ! $order_item->is_type( 'shipping' ) ) {
-					$item_price = $order_item->get_subtotal( 'edit' );
-				}
-			}
 
 			$order_row = [
 				'Item' => [
@@ -145,26 +127,18 @@ class API extends Framework\SV_WC_API_Base {
 					'Type'        => self::ITEM_TYPE_ITEM,
 				],
 				'Quantity' => $this->format_number( $order_item->get_quantity() ),
-				'Price'    => round( $item_price / $order_item->get_quantity(), 7 ),
+				'Price'    => round( ( $order_item->get_total( 'edit' ) ) / $order_item->get_quantity(), 7 ),
 			];
 
-			if ( $order_item->is_type( 'coupon' ) ) {
-				$tax_code = $this->integration->get_matching_tax_code( $order_item->get_tax_class(), null );
+			$item_taxes = $order_item->get_taxes();
+
+			foreach ( $item_taxes['total'] as $tax_id => $amount ) {
+				$tax_code = $this->integration->get_matching_tax_code( null, $tax_id );
 
 				if ( $tax_code ) {
 					$order_row['TaxId'] = $tax_code;
-				}
-			} else {
-				$item_taxes = $order_item->get_taxes();
 
-				foreach ( $item_taxes['total'] as $tax_id => $amount ) {
-					$tax_code = $this->integration->get_matching_tax_code( null, $tax_id );
-
-					if ( $tax_code ) {
-						$order_row['TaxId'] = $tax_code;
-
-						break;
-					}
+					break;
 				}
 			}
 
@@ -172,7 +146,7 @@ class API extends Framework\SV_WC_API_Base {
 				$order_row['TaxId'] = $this->integration->get_matching_tax_code( '' );
 			}
 
-			if ( ( $item_price > 0 || ( $refund && abs( $refund->get_total( 'edit' ) ) > 0 ) ) && $total_tax == 0 ) {
+			if ( ( $order_item->get_total( 'edit' ) > 0 || ( $refund && abs( $refund->get_total( 'edit' ) ) > 0 ) ) && $order_item->get_total_tax() == 0 ) {
 				$order_row['TaxId'] = $this->integration->get_matching_tax_code( 0 );
 
 				if ( ! empty( $account_code = $this->integration->get_option( 'zero_tax_account_code', null ) ) ) {
@@ -180,7 +154,7 @@ class API extends Framework\SV_WC_API_Base {
 				}
 			}
 
-			if ( empty( $order_row['TaxId'] ) && $total_tax >= 0 ) {
+			if ( empty( $order_row['TaxId'] ) && $order_item->get_total_tax() >= 0 ) {
 				$order_row['TaxId'] = $this->integration::DEFAULT_ESTONIAN_TAX_ID;
 			}
 
@@ -236,7 +210,7 @@ class API extends Framework\SV_WC_API_Base {
 				}
 
 				// Check for discounts
-				if ( $order_item->get_subtotal( 'edit' ) != $order_item->get_total( 'edit' ) && ! $has_coupons ) {
+				if ( $order_item->get_subtotal( 'edit' ) != $order_item->get_total( 'edit' ) ) {
 					$discount_amount = $order_item->get_subtotal( 'edit' ) - $order_item->get_total( 'edit' );
 
 					$order_row['DiscountPct']     = $this->format_number( $discount_amount / $order_item->get_subtotal( 'edit' ) * 100 );
@@ -254,25 +228,12 @@ class API extends Framework\SV_WC_API_Base {
 				}
 
 			} else {
+
 				if ( $order_item->is_type( 'shipping' ) ) {
-					$order_row['Item']['Type'] = self::ITEM_TYPE_SERVICE;
 					$order_row['Item']['Code'] = $this->integration->get_option( 'invoice_shipping_sku' );
-
-				} elseif ( $order_item->is_type( 'coupon' ) ) {
-					$_coupon = new \WC_Coupon( $order_item->get_code() );
-
-					if ( 'yes' === $_coupon->get_meta( 'is_coupon_giftcard', true ) ) {
-						$giftcard_sku = $this->integration->get_option( 'invoice_giftcard_sku' );
-
-						if ( $giftcard_sku ) {
-							$order_row['Item']['Code'] = $giftcard_sku;
-						}
-
-						$order_row['Item']['Description'] = sprintf( __( 'Giftcard: %s', 'konekt-merit-aktiva' ), $order_item->get_code() );
-					} else {
-						$order_row['Item']['Description'] = sprintf( __( 'Coupon: %s', 'konekt-merit-aktiva' ), $order_item->get_code() );
-					}
 				}
+
+				$order_row['Item']['Type'] = self::ITEM_TYPE_SERVICE;
 			}
 
 			if ( $refund ) {
@@ -285,19 +246,18 @@ class API extends Framework\SV_WC_API_Base {
 			}
 
 			if ( ! empty( $order_row['DiscountedPrice'] ) ) {
-				$total_amount += $this->format_number( $order_row['DiscountedPrice'] );
+				$total_amount += $this->format_number( $order_row['DiscountedPrice'] * $order_row['Quantity'] );
 			}
 			else {
 				$total_amount += $this->format_number( $order_row['Price'] * $order_row['Quantity'] );
 			}
 
-			$formatted_tax     = $this->format_number( $order_item->is_type( 'coupon' ) ? $total_tax : abs( $total_tax ) );
-			$total_tax_amount += $formatted_tax;
+			$total_tax_amount += $this->format_number( abs( $order_item->get_total_tax( 'edit' ) ) );
 
 			$order_items[] = $order_row;
 			$tax_items[]   = [
 				'TaxId'  => $order_row['TaxId'],
-				'Amount' => $formatted_tax,
+				'Amount' => $this->format_number( abs( $order_item->get_total_tax( 'edit' ) ) ),
 			];
 		}
 
